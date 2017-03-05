ad34adc10522d5f346f4160a57d835f02ee7d539
Brian Seymour <imbrianseymour@gmail.com>
2017-03-05 13:43:07 -0800

    travis test

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,26 @@
 import unittest
+import requests
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_health(self):
+        res = requests.get('http://127.0.0.1:8282/')
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        print res.json()
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+    #def test_upper(self):
+    #    self.assertEqual('foo'.upper(), 'FOO')
+
+    #def test_isupper(self):
+    #    self.assertTrue('FOO'.isupper())
+    #    self.assertFalse('Foo'.isupper())
+
+    #def test_split(self):
+    #    s = 'hello world'
+    #    self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    #    with self.assertRaises(TypeError):
+    #        s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
